53f737c9fcbadba4ac8deacfe8a401ac76b05183
DayDream-21 <vova13158@gmail.com>
2021-10-13 00:13:31 +0300

    Hide usernames and passwords in database  * Created bean dataSourceBean in MyConfig for connection to db  * Replaced inMemoryAuthentication in MySecurityConfig by jdbcAuthentication

--- src/main/java/com/slavamashkov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/slavamashkov/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.slavamashkov.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,28 +8,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    DataSource dataSource;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.
-                        username("slava").
-                        password("slava").
-                        roles("EMPLOYEE")
-                )
-                .withUser(userBuilder.
-                        username("elena").
-                        password("elena").
-                        roles("HR")
-                )
-                .withUser(userBuilder.
-                        username("denis").
-                        password("denis").
-                        roles("MANAGER", "HR")
-                );
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
